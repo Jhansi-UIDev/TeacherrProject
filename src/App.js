a395d710705620b336b6e6e94cdfb4e056f2a9c5
Jhansi Mora <jhansimora@Jhansis-MacBook-Pro.local>
2019-11-14 12:30:00 +0530

    add styles to the layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import ShowDetails from './components/ShowDetails';
 import {Container, Row, Col} from 'reactstrap';
 import {Navbar, Nav} from 'react-bootstrap';
-import image1 from './download.jpeg';
+import image1 from './image3.jpeg';
 import image2 from './image2.jpeg';
 import {FiChevronRight} from 'react-icons/fi';
 
@@ -13,52 +13,42 @@ function App() {
       
         {/* <ShowDetails /> */}
 
-        {/* <div class="wrapper">
-            <header class="header">Header</header>
-            <article class="main">
-              <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
-            </article>
-            <aside class="aside aside-1">Aside 1</aside>
-            <aside class="aside aside-2">Aside 2</aside>
-            <footer class="footer">Footer</footer>
-        </div> */}
         <Navbar bg="dark" variant="dark" expand="lg md">
         <Navbar.Brand href="#home">PSD Websites</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
          <Navbar.Collapse id="basic-navbar-nav">
-         
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">UI Kits </Nav.Link>
-            <Nav.Link href="#features">Premiuim PSD files</Nav.Link>
-            <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="#pricing">Blog</Nav.Link>
-          </Nav>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">UI Kits </Nav.Link>
+              <Nav.Link href="#features">Premiuim PSD files</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Blog</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Navbar>
  
 
         <Container className="container" >
         <Row className="row1">
-              <Col >Awesome free PSD <br/>
-              templates and Resources <br/>
-              www.BlazRobar.com 
-
+              <Col >
+              <div className="row1col1">Awesome free PSD <br/>
+              templates and Resources <br/></div>
+              <span className="url">www.BlazRobar.com </span>  
+              <span ><FiChevronRight className="chevron-style"/></span>
               </Col>
-              
-            </Row>
-            <Row className="row2" noGutters={false}>
-              <Col xs={8} >I am like the Robin Hood of PSD files.</Col>
+             
+        </Row>
+        <Row className="row2" noGutters={false}>
+              <Col xs={8} className="row2col1">I am like the Robin Hood of PSD files.</Col>
               <Col className="row2col2"><FiChevronRight className="chevron-style"/></Col>
               
-            </Row>
-            <Row className="row3" noGutters={false}>
-              <Col md={4}sm={12} className="text-align-center" ><FiChevronRight className="chevron-style"/>
+        </Row>
+        <Row className="row3" noGutters={false}>
+              <Col md={4}sm={12} className="row3col1" ><FiChevronRight className="chevron-style"/>
               </Col>
-              <Col  md={4}sm={12}><img src={image1}/></Col>
-              <Col  md={4}sm={12}><img src={image2}/></Col>
-              
-            </Row>
-            <Row className="row4" noGutters={false}>
+              <Col  md={4}sm={12} className="row3col2"><img src={image1}/></Col>
+              <Col  md={4}sm={12} className="row3col3"><img src={image2}/></Col>
+        </Row>
+        <Row className="row4" noGutters={false}>
             < Col className="aside">
               <div className="inline-block">quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ration <br/><br/>
                   quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ration
@@ -68,12 +58,9 @@ function App() {
                 tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
                 Donec lacinia congue felis in faucibus. <br/><br/>
                 quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ration
-              </div>
-
-              
-            </Col>
-              
-            </Row>
+              </div>    
+             </Col>  
+          </Row>
         </Container>
       
     </div>
